23730dcf62408d58c662704d424f384543e064af
Paolo Miraka <mirakapaolo@gmail.com>
2021-05-24 14:22:43 +0200

    innit changes

---
+++ src/components/EmailForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import addToMailchimp from "gatsby-plugin-mailchimp";
+import * as styles from "./EmailListForm.module.scss";
+
+const EmailListForm = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addToMailchimp(email)
+      .then((data) => {
+        // alert(data.result);
+      })
+      .catch((error) => {
+        // Errors in here are client side
+        // Mailchimp always returns a 200
+      });
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.EmailListForm}>
+      <h2>Subscribe</h2>
+      <div className={styles.Wrapper}>
+        <input
+          placeholder="Email address"
+          name="email"
+          type="text"
+          onChange={handleEmailChange}
+        />
+        <button type="submit">Subscribe</button>
+      </div>
+    </form>
+  );
+};
+
+export default EmailListForm;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
 
 export const IndexPageTemplate = ({
   image,
@@ -16,52 +16,51 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
+    <div className="hero is-halfheight has-background-black">
       <div
+        className="main-text"
         style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          marginTop: "6rem",
+          flexDirection: "column",
         }}
       >
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
+            textAlign: "center",
           }}
         >
           {title}
         </h1>
         <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          className="has-text-weight-normal is-family-primary is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
+            textAlign: "center",
+            fontWeight: "normal",
+            fontSize: "12px !important",
           }}
         >
-          {subheading}
+          {/* {subheading} */}
         </h3>
+        <div className="scroll-indicator">
+          <div className="scroll-container">
+            <div className="chevron"></div>
+            <div className="chevron"></div>
+            <div className="chevron"></div>
+            <span className="text">Discover More</span>
+          </div>
+        </div>
       </div>
     </div>
     <section className="section section--gradient">
@@ -112,7 +111,7 @@ export const IndexPageTemplate = ({
       </div>
     </section>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -124,10 +123,10 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -141,8 +140,8 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,9 +149,9 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -190,4 +189,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
